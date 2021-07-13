c4b58f409de41d7aacc6af66b14b6085b3cd965d
Manik Gupta <manikpokhetra@gmail.com>
2021-07-13 12:03:49 +0530

    Installed and configured ejs layouts and partials

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const port = 8000;
-const app = express();
+const expressLayouts = require('express-ejs-layouts');
 
+const app = express();
+// use static files
+app.use(express.static("assets"));
 // setup ejs
 app.set('views','./views');
 app.set('view engine','ejs');
-
+// use express layouts
+app.use(expressLayouts);
+app.set("layout extractScripts",true);
+app.set("layout extractStyles",true);
 // route for homepage
 app.use('/',require('./routes/index'));
 
