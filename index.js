accfa346b07effc53e2ca25ff41104081d1ff398
Manik Gupta <manikpokhetra@gmail.com>
2021-07-13 12:42:12 +0530

    scss installed and used on homepage

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
-
+const sassMiddleware = require('node-sass-middleware')
 const app = express();
+
+
+// use sass middleware
+app.use(sassMiddleware({
+
+    src : "./assets/scss",
+    dest : "./assets/css",
+    debug: true,
+    outputStyle: 'extended',
+    prefix : '/css'
+}));
+
 // use static files
 app.use(express.static("assets"));
 // setup ejs
