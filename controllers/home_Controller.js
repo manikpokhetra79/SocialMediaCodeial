cdfd010a0338dd67c8df051e4bdc46c1baebe78f
Manik Gupta <manikpokhetra@gmail.com>
2021-08-05 19:47:02 +0530

    likes and friends feature done

--- controllers/home_Controller.js
+++ controllers/home_Controller.js
@@ -16,23 +16,25 @@ module.exports.home = async function(req,res){
         }).populate('likes');
 
         let users = await User.find({});
-        let user = await User.findById(req.user._id)
-        .populate({
-            path : 'friendships',
-            populate : {
-                path : 'from_user',
-                select : ['name',"id","avatar"]
-            }
-        })
-        .populate({
-            path : 'friendships',
-            populate : {
-                path : 'to_user',
-                select : ['name',"id","avatar"]
-            }
-        })
+        let user;
+        if(req.user){
+            user = await User.findById(req.user._id)
+            .populate({
+                path : 'friendships',
+                populate : {
+                    path : 'from_user',
+                    select : ['name',"id","avatar"]
+                }
+            })
+            .populate({
+                path : 'friendships',
+                populate : {
+                    path : 'to_user',
+                    select : ['name',"id","avatar"]
+                }
+            })
+        }
         
-    
         return res.render('home',{
            title: "Codeial | Homepage",
            all_posts : posts,
