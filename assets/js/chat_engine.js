a3e93b71fc3dcd02dad6c7506cb8dab38099a7b4
Manik Gupta <manikpokhetra@gmail.com>
2021-08-07 22:16:43 +0530

    chat files created

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,1 +1,20 @@
 //client side : user
+
+class chatEngine{
+    constructor(chatBoxId,userEmail){
+        this.chatBox = $(`#${chatBoxId}`);
+        this.userEmail = userEmail;
+
+        this.socket = io.connect('http://localhost:5000'); // fires an event connection in config                                               chat_sockets file
+        if(this.userEmail){
+            this.connectionHandler();
+        }
+        // console.log(this.chatBox);
+    }
+
+    connectionHandler(){
+      this.socket.on('connect',function(){  //detects 
+          console.log("conenction established using sockets");
+      });
+    }
+}
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,5 +1,14 @@
 //observer :: server which will recieve incoming requests from subscribers
+// io will handle the connection
+//socket is an object with multiple properties
+module.exports.chatSockets = function(socketServer){ //recieves the conenction
+    let io = require('socket.io')(socketServer);
 
-module.exports.chatSockets = function(socketServer){
+    io.sockets.on('connection',function(socket){
+        console.log("new connection recieved",socket.id);
+    });
 
+    io.sockets.on('disconnect',function(){
+        console.log("Socket disconnected");
+    })
 }
